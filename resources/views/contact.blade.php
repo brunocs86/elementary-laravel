00f0293ad4668f08f11ba67c890f892bddb579d8
Mario Basic <mario.basic@outlook.com>
2016-10-15 16:55:49 +0200

    Blade templates.

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.default')
+
+@section('title', 'Contact page')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="page-header">
+                    <h1>Contact <small>We value all feedback.</small></h1>
+                </div>
+
+                <p class="lead">Feel free to contact us if you have any questions, suggestions or praises.</p>
+
+                {{-- Contact form goes here --}}
+
+            </div>
+        </div>
+    </div>
+@stop
