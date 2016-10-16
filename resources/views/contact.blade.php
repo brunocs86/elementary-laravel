315057e9e97d0d34cde9a683cd00bd5e2dedfdff
Mario Basic <mario.basic@outlook.com>
2016-10-16 13:34:27 +0200

    Created the contact form.

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -12,7 +12,22 @@
 
                 <p class="lead">Feel free to contact us if you have any questions, suggestions or praises.</p>
 
-                {{-- Contact form goes here --}}
+                <form method="POST" action="{{ url('/contact') }}">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder="Your name">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">E-mail</label>
+                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Your E-mail">
+                    </div>
+                    <div class="form-group">
+                        <label for="comment">Message</label>
+                        <textarea rows="10" id="comment" class="form-control" name="comment" placeholder="Your message">{{ old('comment') }}</textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-lg">Send</button>
+                </form>
 
             </div>
         </div>
